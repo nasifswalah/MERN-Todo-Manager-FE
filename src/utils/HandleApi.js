306fb2f3b26edba7706fbaad08e290ae779e3f8f
unknown <nasifswalah@gmail.com>
2024-05-13 11:52:54 +0530

    deploy setup

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:5000"
+const baseURL = "https://todo-manager-be.onrender.com"
 
 const getAlldata = async (setTodo) => {
    await axios
